89f1e42e7311d1ad8e4fb888b903ce7e4edb4f6f
pedroseabra1091 <pedroseabra1091@gmail.com>
2016-04-04 14:38:32 +0100

    minor issues

--- CarDealershipAsService/static/jsx/index.jsx
+++ CarDealershipAsService/static/jsx/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Layout from './layout.jsx';
+import Header from './header.jsx';
+import Body from './body.jsx';
+
+class Mainclass extends React.Component {
+  render(){
+    return(
+      <div>
+        <Header/>
+        <Body/>
+      </div>
+    );
+  }
+}
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>,app);
+ReactDOM.render(<Mainclass/>,app);
